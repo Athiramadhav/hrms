6e63b19263db26587e786f768d55bc87f78bbb67
Athiramadhav <athiramadhavan995@gmail.com>
2019-09-26 13:00:08 -0700

    for pull

--- humanresource/hrapp/urls.py
+++ humanresource/hrapp/urls.py
@@ -8,4 +8,14 @@ urlpatterns =[
 	path('registeremployee',views.home),
 	path('intimation/',views.intimationDetails)
 	#path('exam/',views.addQuestion)
+	path('userlogin/',views.userLogin),
+	path('register/',views.registration),
+	path('candidateregister/', views.candidateRegistration),
+	path('exam/', views.questionPaper),
+	path('mocktest/', views.mockTest),
+	path('projectreg/',views.projectReg),
+    # path('complaintreg/',views.complaintReg)
+
+	# path('registeremployee/',views.)
+	# path('exam/',)
 ]
